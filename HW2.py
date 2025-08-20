6b955cf5e191d48e54ded09aa7a1d17516ca5936
Connor Altland <ca125008@student.wpsok.org>
2025-08-20 14:16:34 -0500

    Added HW#2

--- HW2.py
+++ HW2.py
@@ -7,23 +7,23 @@ from dis import print_instructions
 print("Hello World!")
 #2. Create three different variables with distinct names and values: one with an integer, one with a string, one with a boolean.
 one = 1
-Hurt = ("damage")
-yes = ("yes")
+Hurt = "damage"
+yes = True
 #3. Print all three variables on the same print function (at the same time).
-print("one, Hurt, yes")
+print(one, Hurt, yes)
 #4. Create a variable that asks the user to input an integer.
 Number = int(input("enter a number: "))
 print(Number)
 #5. Add the integer variable from #2 with the integer from #4 and print the result.
-print(one + Number)
+number_add= one + Number
+print(number_add)
 #6. Take the result from #5 and divide it by 2. Print the result.
-divde = one / Number
-print(divde)
+print(number_add /2)
 #7. Change the value of the boolean variable to the opposite value (if true then make false, or vice versa).
-yes = "false"
+yes = False
 #8. Print the value of the boolean variable.
 print(yes)
 #9. Create a variable with a number that contains decimals.
 decimals = 1.5
 #10. Round the number from #9 up or down using the round function.
-print(decimals)
+print(round(decimals))
