09ba0516671997f47cefe4a07b6ca2f0a4fdf1c1
Connor Altland <ca125008@student.wpsok.org>
2025-09-02 14:11:48 -0500

    added HW4

---
+++ HW#4.py
@@ -0,0 +1,32 @@
+#Name:Connor Altland
+#Class: 6th Hour
+#Assignment: HW4
+
+
+#1. Print Hello World!
+print("Hello World")
+#1. Create a list with 5 strings containing 5 different names in it
+str_list = ["Mario","Peach","Toad", "Luigi","Yoshi"]
+print(str_list)
+
+#2. Append a new name onto the Name List.
+str_list.append('Bowser')
+print(str_list)
+#3. Print out the 4th name on the list.
+print(str_list[3])
+#4. Create a list with 4 different integers in it.
+int_list = [10,1,1000,100]
+print(int_list)
+#5. Insert a new integer into the 2nd spot and print the new list.
+int_list.insert(1,2)
+print(int_list.insert)
+#6. Sort the list from lowest to highest and print the sorted list.
+int_list.sort()
+print(int_list)
+#7. Add the 1st three numbers on the sorted list together and print the sum.
+print(sum(int_list))
+#8. Create a list with two strings, two variables, and too boolean values.
+mix_list = ["Bob",9,"false"]
+print(mix_list)
+#9. Create a print statement that asks the user to input their own index value for the list on
+print(mix_list[int(input)()]))
