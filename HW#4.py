8949b8fcc8493559cd14a759cf0129b192183245
Connor Altland <ca125008@student.wpsok.org>
2025-09-03 13:34:57 -0500

    added HW4 fixed the promblem

--- HW#4.py
+++ HW#4.py
@@ -29,4 +29,4 @@ print(sum(int_list))
 mix_list = ["Bob",9,"false"]
 print(mix_list)
 #9. Create a print statement that asks the user to input their own index value for the list on
-print(mix_list[int(input)()]))
+print(mix_list[int(input())])
