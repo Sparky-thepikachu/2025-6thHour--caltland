d7d9629f5f161e7b13b2c44dba1ebf8ab7ec219d
Connor Altland <ca125008@student.wpsok.org>
2025-09-18 14:04:45 -0500

    Playground added

---
+++ playground.py
@@ -0,0 +1,110 @@
+# Name:Connor Altland
+# Class: 6th hour
+#assigment: Playground
+
+import random
+a = random.randint(1,1000)
+b = random.randint(1,1000)
+c = random.randint(1,1000)
+d = random.randint(1,1000)
+e = random.randint(1,1000)
+f = random.randint(1,1000)
+g = random.randint(1,1000)
+h = random.randint(1,1000)
+i = random.randint(1,1000)
+j = random.randint(1,1000)
+k = random.randint(1,1000)
+l = random.randint(1,1000)
+m = random.randint(1,1000)
+n = random.randint(1,1000)
+o = random.randint(1,1000)
+p = random.randint(1,1000)
+q = random.randint(1,1000)
+r = random.randint(1,1000)
+s = random.randint(1,1000)
+t = random.randint(1,1000)
+u = random.randint(1,1000)
+v = random.randint(1,1000)
+w = random.randint(1,1000)
+x = random.randint(1,1000)
+y = random.randint(1,1000)
+z = random.randint(1,1000)
+print(a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z,)
+
+num_list =[a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z]
+num_list.remove(random.choice (num_list))
+print(num_list)
+
+num_list.remove(random.choice (num_list))
+print(num_list)
+
+num_list.remove(random.choice (num_list))
+print(num_list)
+
+num_list.remove(random.choice (num_list))
+print(num_list)
+
+num_list.remove(random.choice (num_list))
+print(num_list)
+
+num_list.remove(random.choice (num_list))
+print(num_list)
+
+num_list.remove(random.choice (num_list))
+print(num_list)
+
+num_list.remove(random.choice (num_list))
+print(num_list)
+
+num_list.remove(random.choice (num_list))
+print(num_list)
+
+num_list.remove(random.choice (num_list))
+print(num_list)
+
+num_list.remove(random.choice (num_list))
+print(num_list)
+
+num_list.remove(random.choice (num_list))
+print(num_list)
+
+num_list.remove(random.choice (num_list))
+print(num_list)
+
+num_list.remove(random.choice (num_list))
+print(num_list)
+
+num_list.remove(random.choice (num_list))
+print(num_list)
+
+num_list.remove(random.choice (num_list))
+print(num_list)
+
+num_list.remove(random.choice (num_list))
+print(num_list)
+
+num_list.remove(random.choice (num_list))
+print(num_list)
+
+num_list.remove(random.choice (num_list))
+print(num_list)
+
+num_list.remove(random.choice (num_list))
+print(num_list)
+
+num_list.remove(random.choice (num_list))
+print(num_list)
+
+num_list.remove(random.choice (num_list))
+print(num_list)
+
+num_list.remove(random.choice (num_list))
+print(num_list)
+
+num_list.remove(random.choice (num_list))
+print(num_list)
+
+num_list.remove(random.choice (num_list))
+print(num_list)
+
+
