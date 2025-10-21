d51015f954b938d80942cbfe4b17c66d9b7345fd
Connor Altland <ca125008@student.wpsok.org>
2025-10-21 13:55:55 -0500

    finished HW10

---
+++ HW10.py
@@ -0,0 +1,30 @@
+#Name:Connor Altland
+#Class: 6th Hour
+#Assignment: HW10
+from operator import truediv
+
+#1. Print Hello World!
+print("Hello World!")
+#2. Create three different boolean variables named wifi, login, and admin.
+wifi = True
+login = True
+admin = True
+#3. Create a separate integer variable that denotes the number of times
+#someone with admin credentials has logged in.
+admin_login = 2
+#4. Create a nested if statement that checks to see if wifi is true,
+#login is true, and admin is true. If they are all true, print a
+#welcome message and increase the integer variable by one. If one of them
+#is false, print an error message telling them which one they are "missing".
+if wifi == True:
+    if login == True:
+        if admin == True:
+          print("Welcome Admin!")
+          admin_login = 2
+          print(admin_login)
+        else:
+            print("welcome player")
+    else:
+        print("login is incorrect")
+else:
+    print("Wifi is not connected")
