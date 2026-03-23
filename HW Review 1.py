15848cb2b383f546ea378375d3bb3814941c0fd6
ca125008@student.wpsok.org <ca125008@student.wpsok.org>
2026-03-23 14:05:02 -0500

    HW Review 1 done

---
+++ HW Review 1.py
@@ -0,0 +1,50 @@
+#Name:Connor Altland
+#Class: 6th Hour
+#Assignment: HW-R1
+from operator import truediv
+
+
+#1. Print "Hello World!"
+print("Hello World!")
+#2. Create three variables with strings as values.
+a =("Apple")
+print(a)
+b = ("banana")
+print(b)
+c = ("candy")
+print(c)
+#3. Create three variables with booleans as values.
+d  = True
+print(d)
+e = False
+print(e)
+f = False
+print(f)
+#4. Create three variables with integers as values.
+g = 1
+print(g)
+h = 2
+print(h)
+i = 3
+print(i)
+#5. Create a variable that asks for the user's name and prints it.
+username = input("What is your name? ")
+print(username)
+#6. Add together the three variables from #4 and print the result.
+j = g + h + i
+print(j)
+#7. Subtract the second variable from the first from #4 and print the result.
+k = g - h
+print(k)
+#8. Multiply the third variable from #4 to the result of #6 and print the result.
+l = i * j
+print(l)
+#9. Create a variable that asks the user for a number with a decimal (a float).
+decimal_number = int(input("What is your decimal number? "))
+print(decimal_number)
+#10. Round the variable from #9 to the nearest whole number and print the result.
+rounded = round(decimal_number)
+print(rounded)
+#11. Find the remainder (hint: modulo) of 355/113. Print the result.
+m = 355 % 113
+print(m)
